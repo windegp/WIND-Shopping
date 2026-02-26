ebc4325128cdd61c6a9f7b5dcd60ce8878c2477b
WIND Shopping <windegp@gmail.com>
2026-02-26 04:45:12 +0200

    Back

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,7 @@
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth"; // 1. أضفنا استيراد مكتبة الحماية
 
 const firebaseConfig = {
   apiKey: "AIzaSyBIIdkBPaQFHhPLo7Gob7sA1LacaT3E2JE",
@@ -11,9 +12,11 @@ const firebaseConfig = {
   appId: "1:596996130193:web:186c91269249c6c5eb8630"
 };
 
-// منع إعادة تشغيل Firebase في كل مرة يتم فيها عمل Refresh
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+// منع إعادة تشغيل Firebase
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+
 const db = getFirestore(app);
 const storage = getStorage(app);
+const auth = getAuth(app); // 2. أضفنا تعريف الحارس هنا
 
-export { db, storage };
+export { db, storage, auth }; // 3. أضفنا auth في التصدير عشان الملفات التانية تشوفه
